2934dd8b20a89e749a2ba44d374c2ca614eca0ae
RITUJA YADAV <rituja2003@gmail.com>
2023-02-03 21:11:03 +0530

    Time: 43 ms (42.99%), Space: 13.9 MB (66.91%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for i in t:
+            if i not in s or s.count(i)<t.count(i):
+    
+          
+                return i
+                
