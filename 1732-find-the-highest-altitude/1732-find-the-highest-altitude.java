1aba1663e481ee285045e906a6a2c833ebb13242
RITUJA YADAV <rituja2003@gmail.com>
2023-02-16 20:40:36 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (96.61%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int s=0,max=0;
+        for(int i=0;i<gain.length;i++){
+            s=s+gain[i];
+           if(s>max){
+                max=s;
+            }
+        }
+        return max;
+    }
+}
