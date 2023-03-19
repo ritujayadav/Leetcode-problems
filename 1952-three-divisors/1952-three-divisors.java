2f1a55c2788aceccd6c8e28befe2e00482428951
RITUJA YADAV <rituja2003@gmail.com>
2023-03-19 14:33:07 +0530

    Time: 1 ms (74.49%), Space: 40.2 MB (15.01%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isThree(int n) {
-        int count =0;
+        int k=0;
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                count++;
+                
+                k++;
             }
         }
-        if(count==3){
+        if(k==3){
             return true;
         }
-        return false;
+            return false;
+        
     }
 }
+
