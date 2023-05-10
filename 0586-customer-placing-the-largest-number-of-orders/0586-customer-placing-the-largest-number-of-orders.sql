b6fd3493797067e2c1735dd7ffd37578ac8413ec
RITUJA YADAV <rituja2003@gmail.com>
2023-05-10 20:35:00 +0530

    Time: 899 ms (65.46%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,2 +1,6 @@
-# Write your 
-select customer_number from Orders group by customer_number order by count(customer_number) desc limit 1;
+
+select customer_number
+from Orders
+group by customer_number
+order by count(customer_number)
+desc limit 1;
