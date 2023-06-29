8b08aa61c6da3608435ab81dc7ff54ad761d96ed
RITUJA YADAV <rituja2003@gmail.com>
2023-06-29 21:10:41 +0530

    Time: 57 ms (74.23%), Space: 44 MB (58.31%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,11 @@
+var ArrayWrapper = function(nums) {
+    this.arr = nums    
+};
+
+ArrayWrapper.prototype.valueOf = function() { 
+    return this.arr.reduce((a, b) => a+b, 0)
+}
+
+ArrayWrapper.prototype.toString = function() {    
+    return '[' + this.arr + ']'
+}
