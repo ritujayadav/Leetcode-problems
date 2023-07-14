fbee27af62738a990addf7ed772197c3d3b976f8
RITUJA YADAV <rituja2003@gmail.com>
2023-07-14 16:34:30 +0530

    Time: 61 ms (61.11%), Space: 42.1 MB (76.93%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -1,9 +1,9 @@
-var reduce = function(nums, fn, t) {
-    if(nums.length == 0){
+var reduce = function(s, fn, t) {
+    if(s.length == 0){
         return t
     }
-    for(let i = 0; i < nums.length; i++){
-       t = fn(t,nums[i])
+    for(let i = 0; i < s.length; i++){
+       t = fn(t,s[i])
     }
     
     return t
