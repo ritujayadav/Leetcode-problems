9ae9bcbfdd75ce089ad2a66f000f38fcbc0f3d43
RITUJA YADAV <rituja2003@gmail.com>
2023-08-12 20:44:54 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (57.57%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int c=0;
+        for(int i:hours)
+        {
+            if(i>=target)
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+}
