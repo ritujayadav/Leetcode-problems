2ca84c0f61be38e60795092bb8a9008966e19ca4
RITUJA YADAV <rituja2003@gmail.com>
2023-08-14 21:15:54 +0530

    Time: 61 ms (71.68%), Space: 44.6 MB (58.66%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,16 @@
+var createCounter = function(init) {
+    let count = init;
+    
+    function increment () {
+        return ++count
+    }
+    function decrement() {
+        return --count
+    }
+    function reset () {
+        return count = init;
+    }
+    return {
+        increment, decrement, reset
+    }
+};
