a4d5dab12106e57e178be3a9207593bfadaa37b4
RITUJA YADAV <rituja2003@gmail.com>
2023-10-15 13:17:08 +0530

    Time: 26 ms (42.39%), Space: 44.7 MB (8.46%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,32 +1,35 @@
 class Solution {
-    public static int fun(int piles[], int curr) {
-        int total = 0;
-        for (int i = 0; i < piles.length; i++) {
-            total += Math.ceil((double) piles[i] / curr);
-        }
-        return total;
+   public static int fun(int piles[],int curr){
+    int total=0;
+    for(int i=0;i<piles.length;i++){
+        total+=Math.ceil((double)piles[i]/curr);
+        
     }
-
-    public static int maxi(int[] piles) {
-        int f = Integer.MIN_VALUE;
-        for (int i = 0; i < piles.length; i++) {
-            f = Math.max(f, piles[i]);
-        }
-        return f;
-    }
-
-    public int minEatingSpeed(int[] piles, int h) {
-        int l = 1;
-        int hig = maxi(piles);
-        while (l <= hig) {
-            int m = l + (hig - l) / 2;
-            int x = fun(piles, m);
-            if (x <= h) {
-                hig = m - 1; // Update hig when x <= h
-            } else {
-                l = m + 1;  // Update l when x > h
+    
+    return total;
+}
+    
+   public static int maxi(int[] piles){
+        int f=Integer.MIN_VALUE;
+       
+        for(int i=0;i<piles.length;i++){
+      
+                f=Math.max(f,piles[i]);
             }
+    return f;
         }
-        return l;
-    }
+ public int minEatingSpeed(int[] piles, int h) {
+    int l=1;
+     int hig=maxi(piles);
+     while(l<=hig){
+        int m=(l+hig)/2;
+            int x=fun(piles,m);
+            if(x<=h){
+                hig=m-1;
+            }else{
+                l=m+1;
+            }
+        }         
+    return l;
+}
 }
