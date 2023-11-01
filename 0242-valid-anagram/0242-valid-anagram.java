ebd2c4f3a9ef2c48470f3256c82f2cdef2af7e4e
RITUJA YADAV <rituja2003@gmail.com>
2023-11-01 21:50:26 +0530

    Time: 6 ms (41.57%), Space: 43.7 MB (33.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    
+    public static String sort(String x){
+        char sc[]=x.toCharArray();
+           Arrays.sort(sc);
+               return new String(sc);
+    }
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+         
+        return false;    
+        }
+        
+        
+        
+        
+        
+        
+        s=sort(s);
+        t=sort(t);
+        
+        for(int i=0;i<s.length();i++){
+           
+                if(s.charAt(i)!=t.charAt(i)){
+                    return false;
+                }
+            }
+        
+        
+        return true;
+        }
+    }
