31aa13bb6b1242ec93d6d541ca9f398efb65da7b
RITUJA YADAV <rituja2003@gmail.com>
2023-11-06 20:41:06 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (44.41%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -2,15 +2,13 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
          ListNode newn=null;
-         
+          ListNode next;
          while(head!=null){
-             ListNode next=head.next;
+             next=head.next;
              head.next=newn;
              newn=head;
-             head=next;
-             
+             head=next;  
          }
         return newn;
-        
     }
 }
